608d48d8b9ab6c2e63d9ad1e9137e6e0b3d3080f
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-08-21 17:23:49 +0900

    WebAppMVC

---
+++ Day003/MVC/Controllers/FirstController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppControler01.Controllers
+{
+    public class FirstController : Controller
+    {
+        [Route("First")]
+        public IActionResult First()
+        {
+            return View();
+        }
+        //[Route("About")]
+        //public IActionResult About()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ Day003/MVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppControler01.Controllers
+{
+    [Route("/Home")]
+    //[Route("[Controller]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        //[Route("{action}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("About")]
+        //[Route("{action}")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("Number/{id?}")]
+        //[Route("{action}/{id?}")]
+        public int Details(int id)
+        {
+            return id;
+        }
+    }
+}
