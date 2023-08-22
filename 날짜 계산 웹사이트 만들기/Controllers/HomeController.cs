082e0fa660a4a33c87668ce0156fbf78a4acb90d
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-08-22 16:53:53 +0900

    Add files via upload

---
+++ 날짜 계산 웹사이트 만들기/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebQuiz01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(DateTime date1, DateTime date2)
+        {
+            ViewBag.result1 = (date2 - date1).ToString("dd");
+            return View();
+        }
+    }
+}
