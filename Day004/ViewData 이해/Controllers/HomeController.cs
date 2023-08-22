b2c91705b4502cd190f57e7e48ea50bd3379becd
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-08-22 16:55:26 +0900

    Add files via upload

---
+++ Day004/ViewData 이해/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/Day")]
+        public IActionResult Day()
+        {
+            return View();
+        }
+
+    }
+}
