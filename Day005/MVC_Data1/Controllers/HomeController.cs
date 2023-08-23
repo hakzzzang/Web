a4595056723826a82a1f34ca09324bd5d0bdb288
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-08-23 16:52:53 +0900

    Add files via upload

---
+++ Day005/MVC_Data1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Quiz.Models;
+using System.Diagnostics;
+
+namespace MVC_Quiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Result(string v1, string v2)
+        {
+            ViewBag.data1 = v1;
+            ViewBag.data2 = v2;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
