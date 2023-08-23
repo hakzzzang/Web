d72aa8be23fa09fcbb3cdbdf7835a6968bcd6f7d
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-08-23 16:53:40 +0900

    Add files via upload

---
+++ Day005/MVC_Model/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Model1.Models;
+using System.Diagnostics;
+
+namespace MVC_Model1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var st1 = new StudentModel()
+            {
+                ID = 1,
+                Name = "김학수",
+                HP = "010-6510-8518",
+                Major = "정보통신공학과"
+            };
+            ViewBag.stdata1 = st1;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
