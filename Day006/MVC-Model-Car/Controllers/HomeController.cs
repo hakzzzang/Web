a68b4ce405e4b21d687010099cc5621736f52ac6
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-08-24 17:18:42 +0900

    MVC 흐름

---
+++ Day006/MVC-Model-Car/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Model_Car.Models;
+using System.Diagnostics;
+
+namespace MVC_Model_Car.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var Car = new List<carClass>()
+            {
+                new carClass{ID = 1, Name = "포르쉐", Speed = "매우빠름" },
+                new carClass{ID = 2, Name = "아반떼", Speed = "보통"},
+                new carClass{ID = 3, Name = "모닝", Speed = "느림"}
+            };
+            ViewData["data1"] = Car;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day006/MVC-Model-Car/Models/carClass.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Model_Car.Models
+{
+    public class carClass
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Speed { get; set; }
+
+    }
+}
