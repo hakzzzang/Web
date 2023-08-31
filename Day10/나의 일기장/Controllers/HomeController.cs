9136fac8319a9e3703afd034e7a0a8631f0bbc53
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-08-31 17:37:24 +0900

    Add files via upload

---
+++ Day10/나의 일기장/Controllers/HomeController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebAppDiary.Models;
+
+namespace WebAppDiary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DiaryDbContext _context;
+
+        public HomeController(DiaryDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var students = await _context.DiaryList.ToListAsync<DiaryClass>();
+            return View(students);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: 삽입기능
+        [HttpPost]
+        public async Task<IActionResult> Add(DiaryClass diaryData)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.DiaryList.AddAsync(diaryData);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Result", "Home");
+            }
+            return View(diaryData);
+        }
+
+        public IActionResult Result(DiaryClass diaryData)
+        {
+            return View(diaryData);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.DiaryList == null)
+            {
+                return NotFound();
+            }
+
+            var diaryData = await _context.DiaryList.FindAsync(id);
+
+            if (diaryData == null)
+            {
+                return NotFound();
+            }
+            return View(diaryData);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, DiaryClass diary)
+        {
+            if (id != diary.No)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                //_context.Student.Update(std);
+                _context.Update(diary);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(diary);
+
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.DiaryList == null)
+            {
+                return NotFound();
+            }
+
+            var diaryData = await _context.DiaryList.FirstOrDefaultAsync(x => x.No == id);
+
+            if (diaryData == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryData);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.DiaryList == null)
+            {
+                return NotFound();
+            }
+            var diaryData = await _context.DiaryList.FirstOrDefaultAsync((x => x.No == id));
+
+            if (diaryData == null)
+            {
+                return NotFound();
+            }
+            return View(diaryData);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var diaryData = await _context.DiaryList.FindAsync(id);
+            if (diaryData != null)
+            {
+                _context.DiaryList.Remove(diaryData);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day10/나의 일기장/Models/DiaryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAppDiary.Models
+{
+    public class DiaryDbContext : DbContext
+    {
+        public DiaryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<DiaryClass> DiaryList { get; set; }
+
+    }
+}
