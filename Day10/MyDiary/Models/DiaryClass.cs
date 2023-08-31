7d3d7a285658bf7d856658cd3071cef3851d8d55
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-08-31 17:15:46 +0900

    Add files via upload

---
+++ Day10/MyDiary/Models/DiaryClass.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppDiary.Models
+{
+    public class DiaryClass
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int No { get; set; }
+
+        public string Writer { get; set; }
+
+        public string Title { get; set; }
+
+        [Required]
+        public string Contents { get; set; }
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+    }
+}
