f0ffd7a51600d0d8c5cd693618ea277dc6a2d544
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-09-07 17:24:00 +0900

    Add files via upload

---
+++ Project/Project/Program.cs
@@ -0,0 +1,49 @@
+
+using Microsoft.EntityFrameworkCore;
+using WebMiniProject.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession(); //단순 세션
+
+//appsetting.json --> connectionString
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<AppDbContext>(item => item.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//세션기능을 사용 하려면 꼭 작성해야 합니다. build쪽 옵션은 말그대로 옵션 설정이지 정작 동작은 app.UseSession()을 통해서 세션 기능이 동작합니다.
+app.UseSession();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Main}/{id?}");
+
+app.Run();
