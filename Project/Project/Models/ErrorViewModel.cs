eb056fef3d5eac7cb0eea667457411e732fde033
hakzzzang <130206609+hakzzzang@users.noreply.github.com>
2023-09-12 20:57:05 +0900

    Delete Project directory

--- Project/Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebMiniProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
